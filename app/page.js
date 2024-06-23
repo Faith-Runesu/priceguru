f5574907daf281bb1fbaae0b6fd082bac65f8cbe
Kupakwashe Makunura <kupsamos@gmail.com>
2024-06-23 16:17:10 +0200

    Create the typewriter effect

--- app/page.js
+++ app/page.js
@@ -1,27 +1,19 @@
-import HeroCarousel from '@/components/HeroCarousel'
-import SearchBar from '@/components/SearchBar'
+import Header from "@/components/Header";
+import HeroCarousel from "@/components/HeroCarousel";
+import SearchBar from "@/components/SearchBar";
 
 const HomePage = () => {
   return (
-    <section className='px-6 border-2 md:px-20 py-10'>
-      <div className='flex max-xl:flex-col gap-16'>
-        <div className='flex flex-col justify-center'>
-          <p className='small-text'>
-            Start shopping smarter today
-          </p>
-          <h1 className='head-text'>
-            For the right prices, trust the <span className='text-primary'>Guru</span>
-          </h1>
-          <p className='mt-6'>
-            Price comparison for tech and gadgets made easy, start saving with us today
-          </p>
+    <section className="px-6 border-2 md:px-20 py-5">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center space-y-2">
+          <Header />
           <SearchBar />
         </div>
-        <HeroCarousel/>
+        <HeroCarousel />
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
---
+++ components/Header.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { useTypewriter } from "react-simple-typewriter";
+
+const Header = () => {
+  const words = ["For", "the", "right", "prices,", "trust", "the", "Guru"];
+  const fullSentence = words.join(' ')
+
+  const [text] = useTypewriter({
+    words: [fullSentence],
+    loop: 0,
+  });
+
+  // Split the text to separate the last word
+  const textParts = text.split(' ');
+  const lastWord = textParts.pop();
+  const sentenceWithoutLastWord = textParts.join(' ');
+  return (
+    <>
+      <p className="small-text">Start shopping smarter today</p>
+      <h1 className="text-5xl">
+        {sentenceWithoutLastWord}
+        <span className="text-primary">{lastWord}</span>
+      </h1>
+      <p className="mt-6">
+        Price comparison for tech and gadgets made easy, start saving with us
+        today
+      </p>
+    </>
+  );
+};
+
+export default Header;
