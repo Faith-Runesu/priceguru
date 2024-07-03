88fc6c399f02702a3025aba72fd2575625c00033
St <aaronndas@gmail.com>
2024-07-03 18:31:10 +0200

    Cron 1

---
+++ app/api/cron/route.js
@@ -0,0 +1,44 @@
+import Tracker from "@/lib/models/tracker.model";
+import { connectToDB } from "@/lib/mongoose";
+import { emailBody, sendMail } from "../nodemailer";
+import mongoose from "mongoose";
+import { scrapedOneFromLaptopZone } from "@/lib/scrapping_engine_LP";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    updatedProducts = []
+    try{
+        await connectToDB();
+        let updated = false;
+        const trackers = await Tracker.find({});
+        //scrape using tracker
+        for (let i = 0; i < trackers.length; i++) {
+            let track = trackers[i];
+            if (track.link.startWith("https://everythingzimbabwean.com")) {
+                updated = await scrapedOneFromEverythingZimbabwe(track.link);
+            } else if (track.link.startWith("https://laptopzone.co.zw")) {
+                updated = await scrapedOneFromLaptopZone(track.link);
+            }
+            if (updated.price){
+                let newTracker = {
+                    price: updated.price,
+                }
+                await Tracker.findByIdAndUpdate(track._id, newTracker);
+                if (updated.price !== track.price) {
+                    updatedProducts.push(updated)
+                    const emailContent = emailBody(updated, "PRICECHANGE");
+                    for (let j = 0; j < track.emails.length; j++) {
+                        await sendMail(emailContent, track.emails[i]);
+                    }
+                }
+
+            }
+        }
+        return NextResponse.json({
+            message: 'Ok',
+            data: updatedProducts,
+        })
+    } catch (error) {
+        throw new Error(`Error in GET request ${error}`)
+    }
+}
